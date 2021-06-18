e358647da52cef6fe407173dc3f3b6cbf25d4f32
Mateus de Sousa Silva Santos <54694745+mateuso12@users.noreply.github.com>
2021-06-17 21:19:47 -0300

    Criando componente e páginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton/Index";
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -6,7 +9,20 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Eae, bem-vindo</span>
+          <h1>Noticias sobre o mundo do <span>React</span></h1>
+          <p>
+            Tenha acesso a todas as publicações <br />
+            <span>por R$ 15,99 / mês</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Mulher codando"/>
+      </main>
     </>
   );
 }
