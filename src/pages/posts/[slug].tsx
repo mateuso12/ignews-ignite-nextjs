aedf66b7196165646e6d27638346b5c139d4710c
Mateus de Sousa Silva Santos <54694745+mateuso12@users.noreply.github.com>
2021-07-16 21:31:55 -0300

    Criando html da página de post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,7 +1,9 @@
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/client";
+import Head from "next/head";
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
+import styles from './post.module.scss'
 
 interface PostProps {
   slug: string;
@@ -11,43 +13,57 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
-  
-  return(
-    <h1>teste</h1>
-    );
-  }
-  export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
 
-    const session = await getSession({ req })
-    const { slug } = params;
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+          className={styles.postContent} 
+          dangerouslySetInnerHTML={{ __html: post.content }} 
+          />
+        </article>
+      </main>
+    </>
+  );
+}
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const session = await getSession({ req });
+  const { slug } = params;
 
-    // if (!session) {
+  // if (!session) {
 
-    // }
+  // }
 
+  const prismic = getPrismicClient(req);
 
-    const prismic = getPrismicClient( req );
+  const response = await prismic.getByUID("post", String(slug), {});
 
-    const response = await prismic.getByUID('post', String(slug), {});
-    
-    const post = {
-      slug,
-      title: RichText.asText(response.data.title),
-      content: RichText.asHtml(response.data.content),
-      updatedAt: new Date(response.last_publication_date).toLocaleDateString(
-        "pt-BR",
-        {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }
-      )
-    }
-
-    return {
-      props: {
-        post,
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
       }
-    }
+    ),
+  };
 
-  }
+  return {
+    props: {
+      post,
+    },
+  };
+};
