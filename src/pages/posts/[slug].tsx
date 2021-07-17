7b51795ddd1fb1947f80def5185f0e8ab3de3b65
Mateus de Sousa Silva Santos <54694745+mateuso12@users.noreply.github.com>
2021-07-16 22:30:54 -0300

    Verificando assinatura do usuário

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -3,13 +3,15 @@ import { getSession } from "next-auth/client";
 import Head from "next/head";
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
-import styles from './post.module.scss'
+import styles from "./post.module.scss";
 
 interface PostProps {
-  slug: string;
-  title: string;
-  content: string;
-  updatedAt: string;
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
 }
 
 export default function Post({ post }: PostProps) {
@@ -24,8 +26,8 @@ export default function Post({ post }: PostProps) {
           <h1>{post.title}</h1>
           <time>{post.updatedAt}</time>
           <div
-          className={styles.postContent} 
-          dangerouslySetInnerHTML={{ __html: post.content }} 
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.content }}
           />
         </article>
       </main>
