b59927def1d9257ad48aab7742e63047ff4a819c
Mateus de Sousa Silva Santos <54694745+mateuso12@users.noreply.github.com>
2021-07-19 17:16:50 -0300

    Inscrição página preview e verificação de usuário

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps } from "next";
-import { getSession } from "next-auth/client";
+import { getSession, useSession } from "next-auth/client";
 import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { RichText } from "prismic-dom";
+import { useEffect } from "react";
 import { getPrismicClient } from "../../../services/prismic";
 import styles from "../post.module.scss";
 
@@ -15,6 +18,15 @@ interface PostPreviewProps {
 }
 
 export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if(session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`)
+    }
+  }, [session]) 
+
   return (
     <>
       <Head>
@@ -29,6 +41,13 @@ export default function PostPreview({ post }: PostPreviewProps) {
             className={`${styles.postContent} ${styles.previewContent}`}
             dangerouslySetInnerHTML={{ __html: post.content }}
           />
+
+          <div className="continueReading">
+            Deseja continuar lendo?
+            <Link href="/">
+            <a href="">Inscreva-se agora 🤗 </a>
+            </Link>
+          </div>
         </article>
       </main>
     </>
